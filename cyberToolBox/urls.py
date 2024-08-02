241056a62c7b8dd0c4d04663d6c724a4e914e6b0
Khursaid Ansari <ansari.khursaid7tr@gmail.com>
2024-08-02 17:06:20 -0500

    updated readme

--- cyberToolBox/urls.py
+++ cyberToolBox/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +31,5 @@ urlpatterns = [
     path('dns-lookup/', include('dns_lookup.urls')),
     path('ssl-checker/', include('ssl_checker.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
