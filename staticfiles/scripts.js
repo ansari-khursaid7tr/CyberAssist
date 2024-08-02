5e6c4dd57606ab71598b30a51b1b4bec1e8b0c05
Khursaid Ansari <ansari.khursaid7tr@gmail.com>
2024-08-02 18:57:30 -0500

    updated readme

--- staticfiles/scripts.js
+++ staticfiles/scripts.js
@@ -11,4 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
+
+    var menuToggle = document.querySelector('.menu-toggle');
+    var navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
 });
