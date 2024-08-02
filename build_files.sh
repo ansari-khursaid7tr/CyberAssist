7fc9c3469374e73bc61a6561f393960afc566a1c
Khursaid Ansari <ansari.khursaid7tr@gmail.com>
2024-08-02 18:44:28 -0500

    updated readme

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-set -e
-set -x
-
 echo "Building project packages..."
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 
 echo "Collecting static files..."
-#python3 manage.py collectstatic
+python3 manage.py collectstatic --no-input
 
 echo "Build script completed successfully"
