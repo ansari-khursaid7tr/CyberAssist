ef40dd3f2b7a7798d3f2591c6e402b0e1de65e92
Khursaid Ansari <ansari.khursaid7tr@gmail.com>
2024-08-02 17:08:48 -0500

    updated readme

--- cyberToolBox/settings.py
+++ cyberToolBox/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4(d^_ew_0s^zd4mrfbw&j$yms^k_2=y#xtfnf$ti(=3tzhd6rg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh", "localhost", "127.0.0.1"]
 
 
 # Application definition
