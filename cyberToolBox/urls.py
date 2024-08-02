46ade4f94cfa5affce36123df30bb7d68c475f81
Khursaid Ansari <ansari.khursaid7tr@gmail.com>
2024-08-02 17:28:23 -0500

    updated readme

--- cyberToolBox/urls.py
+++ cyberToolBox/urls.py
@@ -32,4 +32,5 @@ urlpatterns = [
     path('ssl-checker/', include('ssl_checker.urls')),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
