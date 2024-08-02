6fec5b71931dfe7cbf152e65183d07495c82060d
Khursaid Ansari <ansari.khursaid7tr@gmail.com>
2024-08-02 17:15:14 -0500

    updated readme

--- cyberToolBox/settings.py
+++ cyberToolBox/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Add this at the end of the file
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
